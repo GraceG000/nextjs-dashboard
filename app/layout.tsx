4dd03edca7bf4e6aaace3e7c1877ab673376ac70
Grace Ghansah <gracegh@stlghana.com>
2024-03-25 14:50:55 +0000

    optimizing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 //importing the global.css file...
 import '@/app/ui/global.css';
+//importing the new font...
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -8,7 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/*using the font, so that it will be applied throughout the application...antialiased is a Tailwing class which
+      smooths out the font. It is optional, but antialiased adds a nice touch...*/}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
