1d4c8973cec45b9af74b4f205fc72b8b23076f30
Grace Ghansah <gracegh@stlghana.com>
2024-03-25 17:53:34 +0000

    working with images...image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import styles from './ui/home.module.css';
 //importing the lusitana font...
 import { lusitana } from './ui/fonts';
+//importing the Image component for image optimization...
+import  Image  from 'next/image';
 
 export default function Page() {
   return (
@@ -36,6 +38,24 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/* Adding an image for the desktop orientation... */}
+          <Image
+             src="/hero-desktop.png"
+             width={1000}
+             height={760}
+             className='hidden md:block'
+             alt="Screenshots of the dashboard project shoowing desktop version"
+          />
+          
+         {/* This image will show on the mobile view... */}
+          <Image
+            src="/hero-mobile.png"
+            width = {560}
+            height={620}
+            className='block md:hidden'
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
+
         </div>
       </div>
     </main>
