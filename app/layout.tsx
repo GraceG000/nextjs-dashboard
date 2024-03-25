5efc3495a85f6a733137cf76ddaeef2677782a32
Grace Ghansah <gracegh@stlghana.com>
2024-03-25 11:50:32 +0000

    NextJS Dashboard (Finance) Application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+//importing the global.css file...
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,10 +1,12 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+//importing clsx to toggle between styles, based on invoice status--pending or paid...
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
+//using clsx to toggle class names...I am using this to toggle between styles based on the invoice status--pending or paid...
+  className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
